9b911969bc5f45da0dd2f4a7ab797e495f050a40
Claire SAY <shimizouille@gmail.com>
2025-08-21 00:22:48 +0200

    initialisation des pages + mise en place de la navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { Projects } from './pages/projects/projects';
+import { Skills } from './pages/skills/skills';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'projects', component: Projects },
+    { path: 'skills', component: Skills },
+    { path: 'contact', component: Contact },
+    { path: '**', redirectTo: '' } // fallback
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
